db048a1b40732ec5782077505cd2eb3a25983cfe
Andrea Lisa <andrea.lisaa@gmail.com>
2019-09-22 16:49:41 +0200

    hooks on WeatherApp

--- src/Pages/WeatherApp.js
+++ src/Pages/WeatherApp.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Components/header/header';
 import Search from '../Components/Search/search';
 import ResultCard from '../Components/Cards/ResultCard/resultCard';
 import Wrapper from '../Components/wrapper';
 import { ThemeProvider } from 'styled-components';
-import styled from 'styled-components';
 import Clouds from '../assets/images/cloudy.jpg';
 
 const themeDark = {
@@ -19,80 +18,72 @@ const themeLight = {
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class App extends React.Component {
-  state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    main: undefined,
-    error: undefined
-  }
-
-  setIcon = () => {
-    if (this.state.main === 'Clear') {
-      return <img src="../assets/images/sun.jpg" alt={this.state.description} />
+const App = () => {
+  const [temperature, setTemperature] = useState(undefined);
+  const [city, setCity] = useState(undefined);
+  const [country, setCountry] = useState(undefined);
+  const [description, setDescription] = useState(undefined);
+  const [main, setMain] = useState(undefined);
+  const [error, setError] = useState(undefined)
+  
+  const setIcon = () => {
+    if (main === 'Clear') {
+      return <img src="../assets/images/sun.jpg" alt={description} />
     }
-    if (this.state.main === "Rain"){
-      return <img src={Clouds} alt={this.state.description} />
+    if (main === "Rain"){
+      return <img src={Clouds} alt={description} />
     }
   }
 
-  getWeather = async (e) =>{
+  const getWeather = async (e) =>{
     e.preventDefault();
     const city = e.target.elements.city.value;
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`);
     const dataJson = await apiCall.json(); //transform data into json format
     console.log(dataJson);
     if (dataJson.city) {
-    this.setState({
-      temperature: Math.round(dataJson.list[0].main.temp/10),
-      city: dataJson.city.name,
-      country: dataJson.city.country,
-      humidity: dataJson.list[0].main.humidity,
-      main: dataJson.list[0].weather[0].main,
-      description: dataJson.list[0].weather[0].description,
-      error: ''
-    })}
-
-    else {
-      this.setState({
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: 'Please enter a valid city name'
-    })
-  }
+      return(
+      setTemperature(Math.round(dataJson.list[0].main.temp/10)),
+      setCity(dataJson.city.name),
+      setCountry(dataJson.city.country),
+      setMain (dataJson.list[0].weather[0].main),
+      setDescription( dataJson.list[0].weather[0].description),
+      setError('')
+      )
+    }else {
+      return (
+      setTemperature(undefined),
+      setCity(undefined),
+      setCountry(undefined),
+      setMain (undefined),
+      setDescription(undefined),
+      setError('')
+      )
+    }
   }
 
-  render (){
-    return(
-    <ThemeProvider theme={themeLight}>
-      <Wrapper>
-        <ThemeProvider theme={themeDark}>
-          <Header />
-        </ThemeProvider>
-        <ThemeProvider theme={themeLight}>
-          <Search getWeather={this.getWeather}/>
-        </ThemeProvider>
-        <ThemeProvider theme={themeDark}>
-          <ResultCard 
-            temperature={this.state.temperature}
-            city={this.state.city}
-            country={this.state.country}
-            humidity={this.state.humidity}
-            description={this.state.description}
-            icon={this.setIcon()}
-            error={this.state.error}
-            />
-        </ThemeProvider>
-      </Wrapper>
-    </ThemeProvider>
-    )
-  }
+  return(
+  <ThemeProvider theme={themeLight}>
+    <Wrapper>
+      <ThemeProvider theme={themeDark}>
+        <Header />
+      </ThemeProvider>
+      <ThemeProvider theme={themeLight}>
+        <Search getWeather={getWeather}/>
+      </ThemeProvider>
+      <ThemeProvider theme={themeDark}>
+        <ResultCard 
+          temperature={temperature}
+          city={city}
+          country={country}
+          description={description}
+          icon={setIcon()}
+          error={error}
+          />
+      </ThemeProvider>
+    </Wrapper>
+  </ThemeProvider>
+  )
 }
 
 export default App;
